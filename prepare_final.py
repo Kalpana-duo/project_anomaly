6a2d189ce6c80abb495eab42905623206ef53f28
Mijail Q Mariano <mijail.mariano@gmail.com>
2022-08-25 10:53:37 -0400

    final initial push 25AUG22

--- prepare_final.py
+++ prepare_final.py
@@ -1,58 +1,7 @@
 # .py file dependencies
-import os
+
 import pandas as pd
 import numpy as np
-
-from skimpy import clean_columns
-
-import env_mm
-from env_mm import user, password, host, get_connection
-
-
-'''Function to retrieve Codeup Curriculum Logs and cache as .csv file'''
-def get_logs_dataset():
-
-    # creating the operating system filename for referencing
-    filename = "curriculum_logs.csv"
-    if os.path.isfile(filename):
-        
-        df = pd.read_csv(filename)
-
-        # let's print the shape
-        print(f'df shape: {df.shape}')
-
-        return df
-
-    else: 
-
-        # creating the corriculum logs url for to retrieve from MySQL
-        url = f'mysql+pymysql://{env_mm.user}:{env_mm.password}@{env_mm.host}/curriculum_logs'
-
-        # creating the MySQL query
-        query = '''
-                SELECT 
-                date, 
-                time,
-                path as endpoint,
-                user_id,
-                cohort_id,
-                ip,
-                name,
-                slack,
-                start_date,
-                end_date,
-                program_id,
-                FROM curriculum_logs.logs
-                LEFT JOIN curriculum_logs.cohorts ON curriculum_logs.logs.cohort_id = curriculum_logs.cohorts.id;
-                '''
-
-        # creating the df
-        df = pd.read_sql(query, url)
-
-        # cache the df to local repository 
-        df.to_csv("curriculum_logs.csv", index = False)
-
-        return df
 
 
 '''function that takes in the dataframe and sets the date & time as
